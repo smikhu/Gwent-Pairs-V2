b912ac978e9ac6e4591071a84312a9453d159888
Steven Mikhu <StevenMikhu@gmail.com>
2023-04-08 00:17:56 -0400

    lives and matched counter updating properly

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ let firstChoice = null;
 let secondChoice = null;
 let numFlipped = 0;
 
+let cardsMatched = document.querySelector("#matched");
+let matched = 0;
+
+let livesLeft = document.querySelector("#lives");
+let lives = 7;
+
 function shuffle() {
   cards.forEach((card) => {
     let mixAndMatch = Math.floor(Math.random() * 16);
@@ -16,15 +22,20 @@ function shuffle() {
 shuffle();
 
 function flipCard(event) {
-  // Only allow the user to flip cards if less than two cards are flipped
   if (numFlipped < 2) {
+    let card = event.target;
+    if (card.dataset.matched === "true") {
+      return;
+    }
     if (firstChoice === null) {
-      firstChoice = event.target;
+      firstChoice = card;
       firstChoice.classList.add("flipped");
       firstChoice.src = `./images/${this.dataset.name}.jpg`;
       numFlipped++;
-    } else if (secondChoice === null) {
-      secondChoice = event.target;
+      // Remove click event listener from the firstChoice element.
+      firstChoice.removeEventListener("click", flipCard);
+    } else if (secondChoice === null && card !== firstChoice) {
+      secondChoice = card;
       secondChoice.classList.add("flipped");
       secondChoice.src = `./images/${this.dataset.name}.jpg`;
       numFlipped++;
@@ -35,6 +46,9 @@ function flipCard(event) {
 
 function checkForMatch() {
   if (firstChoice.dataset.name === secondChoice.dataset.name) {
+    firstChoice.dataset.matched = "true";
+    secondChoice.dataset.matched = "true";
+    matchedCounter();
     firstChoice = null;
     secondChoice = null;
     numFlipped = 0;
@@ -42,13 +56,24 @@ function checkForMatch() {
     setTimeout(function () {
       firstChoice.classList.remove("flipped");
       secondChoice.classList.remove("flipped");
-      console.log(firstChoice)
-      console.log(secondChoice)
       firstChoice.src = "./images/back.jpg";
       secondChoice.src = "./images/back.jpg";
       numFlipped = 0;
       firstChoice = null;
       secondChoice = null;
+      livesCounter();
     }, 1000);
   }
+}
+
+function matchedCounter() {
+  matched++;
+  cardsMatched.innerHTML = matched;
+  matched >= 8 ? (cardsMatched.style.color = "green") : "";
+}
+
+function livesCounter() {
+  lives--;
+  livesLeft.innerHTML = lives;
+  lives <= 0 ? (livesLeft.style.color = "red") : "";
 }
