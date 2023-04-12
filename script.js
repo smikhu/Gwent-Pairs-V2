4b4a5d839b500d517ef1263386d07dd78c4d8036
Steven Mikhu <StevenMikhu@gmail.com>
2023-04-11 22:18:40 -0400

    cards being reset properly, data-matched and src being changed properly

--- script.js
+++ script.js
@@ -24,6 +24,13 @@ let gameOverMenu = document.querySelector("#gameOver");
 
 let winGameMenu = document.querySelector("#winGame");
 
+// Variable for reset buttons
+
+let resetButton = document.querySelectorAll(".btn");
+resetButton.forEach((resetButton) => {
+  resetButton.addEventListener("click", reset);
+});
+
 // Audio controls
 
 const sounds = {
@@ -35,12 +42,16 @@ const sounds = {
   background: new Audio("audio/LullabyOfWoe.mp3"),
 };
 
-sounds.background.volume = 0.3;
+sounds.background.volume = 0.1;
 sounds.flip.volume = 0.3;
 sounds.incorrect.volume = 0.2;
 sounds.correct.volume = 0.2;
 sounds.lose.volume = 0.3;
 sounds.win.volume = 0.3;
+
+shuffle();
+
+// playMusic();
 
 // Shuffle function that shuffles all the cards in a random order at the start of the game
 
@@ -50,10 +61,6 @@ function shuffle() {
     card.style.order = mixAndMatch;
   });
 }
-
-shuffle();
-
-playMusic();
 
 function flipCard(event) {
   if (numFlipped < 2) {
@@ -91,9 +98,9 @@ function checkForMatch() {
       matchedCounter();
       sounds.correct.currentTime = 0;
       sounds.correct.play();
+      numFlipped = 0;
       firstChoice = null;
       secondChoice = null;
-      numFlipped = 0;
     }, 500);
   } else {
     setTimeout(function () {
@@ -116,8 +123,8 @@ function checkForMatch() {
 function matchedCounter() {
   matched++;
   cardsMatched.innerHTML = matched;
-  matched >= 8 ? (cardsMatched.style.color = "green") : "";
-  if (matched == 8 && matched == cards.length / 2) {
+  matched === 8 ? (cardsMatched.style.color = "green") : "";
+  if (matched === 8 && matched == cards.length / 2) {
     cards.forEach((card) => card.removeEventListener("click", flipCard));
     setWinState();
     sounds.win.play();
@@ -129,8 +136,8 @@ function matchedCounter() {
 function livesCounter() {
   lives--;
   livesLeft.innerHTML = lives;
-  lives <= 0 ? (livesLeft.style.color = "red") : "";
-  if (lives <= 0) {
+  lives === 0 ? (livesLeft.style.color = "red") : "";
+  if (lives === 0) {
     cards.forEach((card) => card.removeEventListener("click", flipCard));
     setLoseState();
     sounds.lose.play();
@@ -156,6 +163,56 @@ function setLoseState() {
 // Function for background music
 
 function playMusic() {
-  let backgroundMusic = sounds.background.play();
+  let backgroundMusic = sounds.background;
   backgroundMusic.play();
 }
+
+// Function for new game and reset
+
+function reset() {
+  // flip all cards back over
+  cards.forEach((card) => {
+    card.classList.remove("flipped");
+    card.dataset.matched = "false";
+    card.src = "./images/back.jpg";
+    card.addEventListener("click", flipCard);
+  });
+
+  const cardNames = document.querySelectorAll(".card img");
+  cardNames.forEach((card) => {
+    card.src = "./images/back.jpg";
+    card.classList.remove("flipped");
+    card.removeAttribute("data-matched");
+    card.addEventListener("click", flipCard);
+  });
+
+  // reset game variables
+  firstChoice = null;
+  secondChoice = null;
+  numFlipped = 0;
+  matched = 0;
+  lives = 7;
+
+  const menuVars = [
+    { element: cardsMatched, value: matched, color: "white" },
+    { element: livesLeft, value: lives, color: "white" },
+  ];
+
+  menuVars.forEach((varObj) => {
+    varObj.element.style.color = varObj.color;
+    varObj.element.textContent = varObj.value;
+  });
+
+  // hide game over and win game menus
+  gameOverMenu.classList.remove("show");
+  winGameMenu.classList.remove("show");
+
+  // shuffle cards and play background music
+
+  shuffle();
+
+  // playMusic();
+}
+
+
+// FIX THE CARDS STILL ABLE TO BE FLIPPED OVER AFTER YOU RESET AND LOSE THE GAME*****
