244f461ff78157f7b099d1148cc0e06db41b6bc2
Steven Mikhu <StevenMikhu@gmail.com>
2023-04-14 01:02:41 -0400

    hud menu remodeled

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ let numFlipped = 0;
 
 // Selectors and variables for the menu
 
-let cardsMatched = document.querySelector("#matched");
-let matched = 0;
+let lifeBar = document.querySelector(".bar-decrement");
+let matchBar = document.querySelector(".bar-increment");
 
-let livesLeft = document.querySelector("#lives");
-let lives = 7;
+let lifeContainer = document.querySelector(".life");
+let matchContainer = document.querySelector(".match");
 
 // Variables for gameOver and winGame menu
 
@@ -39,8 +39,11 @@ resetButton.forEach((resetButton) => {
 
 // Audio controls
 
+let mainTheme = document.querySelector("#background-music");
+
 const sounds = {
   flip: new Audio("audio/cardflip.mp3"),
+  potion: new Audio("audio/potion.mp3"),
   incorrect: new Audio("audio/incorrect.mp3"),
   correct: new Audio("audio/correct.mp3"),
   lose: new Audio("audio/dead.mp3"),
@@ -50,6 +53,7 @@ const sounds = {
 
 sounds.background.volume = 0.1;
 sounds.flip.volume = 0.2;
+sounds.potion.volume = 0.2;
 sounds.incorrect.volume = 0.2;
 sounds.correct.volume = 0.2;
 sounds.lose.volume = 0.3;
@@ -72,7 +76,7 @@ function startGame() {
 
     shuffle();
 
-    playMusic();
+    // playMusic();
 
     cards.forEach((card) => card.addEventListener("click", flipCard));
 
@@ -98,8 +102,6 @@ function flipCard(event) {
       sounds.flip.play();
       firstChoice.src = `./images/${this.dataset.name}.jpg`;
       numFlipped++;
-      // Remove click event listener from the firstChoice element.
-      //   firstChoice.removeEventListener("click", flipCard);
       // Conditional below prevents the firstChoice card from being the secondChoice card as well
     } else if (secondChoice === null && card !== firstChoice) {
       secondChoice = card;
@@ -118,7 +120,7 @@ function checkForMatch() {
     setTimeout(function () {
       firstChoice.dataset.matched = "true";
       secondChoice.dataset.matched = "true";
-      matchedCounter();
+      increaseWidth();
       sounds.correct.currentTime = 0;
       sounds.correct.play();
       numFlipped = 0;
@@ -134,36 +136,41 @@ function checkForMatch() {
       numFlipped = 0;
       firstChoice = null;
       secondChoice = null;
-      livesCounter();
+      decreaseWidth();
       sounds.incorrect.currentTime = 0;
       sounds.incorrect.play();
+      sounds.potion.currentTime = 0;
+      sounds.potion.play();
     }, 1000);
   }
 }
 
-function matchedCounter() {
-  matched++;
-  cardsMatched.textContent = matched;
-  cardsMatched.style.color =
-    matched === 8 ? "green" : matched >= 5 ? "yellow" : "";
-  if (matched === 8 && matched == cards.length / 2) {
-    cards.forEach((card) => card.removeEventListener("click", flipCard));
-    setWinState();
-    sounds.win.play();
-  }
-}
-
-// Decrement function for lives remaining
-
-function livesCounter() {
-  lives--;
-  livesLeft.textContent = lives;
-  livesLeft.style.color = lives <= 0 ? "red" : lives <= 4 ? "orange" : "";
-  if (lives <= 0) {
+function decreaseWidth() {
+  let bar = document.querySelector(".bar-decrement");
+  let currentWidth = parseInt(bar.style.width) || 100;
+  let newWidth = currentWidth - 14.29;
+  if (newWidth <= 0) {
+    lifeContainer.style.boxShadow = "0px 0px 10px 5px #ff0000cc";
     cards.forEach((card) => card.removeEventListener("click", flipCard));
     setLoseState();
     sounds.lose.play();
+    newWidth = 0;
   }
+  bar.style.width = newWidth + "%";
+}
+
+function increaseWidth() {
+  let bar = document.querySelector(".bar-increment");
+  let currentWidth = parseInt(bar.style.width) || 0;
+  let newWidth = currentWidth + 13.5;
+  if (newWidth >= 100) {
+    matchContainer.style.boxShadow = "0px 0px 5px 2px #00FF00";
+    cards.forEach((card) => card.removeEventListener("click", flipCard));
+    setWinState();
+    sounds.win.play();
+    newWidth = 100;
+  }
+  bar.style.width = newWidth + "%";
 }
 
 function setStartState() {
@@ -173,13 +180,13 @@ function setStartState() {
       card.src = `./images/${card.dataset.name}.jpg`;
       card.removeEventListener("click", flipCard);
     }
-    startGameMenu.style.visibility = "visible";
-    startGameMenu.classList.add("show");
+    // startGameMenu.style.visibility = "visible";
+    // startGameMenu.classList.add("show");
   });
 }
 
 function setWinState() {
-  sounds.background.pause();
+  // sounds.background.pause();
   setTimeout(function () {
     winGameMenu.style.visibility = "visible";
     winGameMenu.classList.add("show");
@@ -193,7 +200,7 @@ function setLoseState() {
       card.src = `./images/${card.dataset.name}.jpg`;
     }
   });
-  sounds.background.pause();
+  // sounds.background.pause();
   setTimeout(function () {
     gameOverMenu.style.visibility = "visible";
     gameOverMenu.classList.add("show");
@@ -201,7 +208,8 @@ function setLoseState() {
 }
 
 function playMusic() {
-  let backgroundMusic = sounds.background;
+  let backgroundMusic = mainTheme;
+  backgroundMusic.volume = 0.1;
   backgroundMusic.play();
 }
 
@@ -221,18 +229,10 @@ function reset() {
     firstChoice = null;
     secondChoice = null;
     numFlipped = 0;
-    matched = 0;
-    lives = 7;
-
-    const menuVars = [
-      { element: cardsMatched, value: matched, color: "white" },
-      { element: livesLeft, value: lives, color: "white" },
-    ];
-
-    menuVars.forEach((varObj) => {
-      varObj.element.style.color = varObj.color;
-      varObj.element.textContent = varObj.value;
-    });
+    lifeBar.style.width = "100%";
+    matchBar.style.width = "0%";
+    matchContainer.style.boxShadow = "none";
+    lifeContainer.style.boxShadow = "none";
 
     // hide game over and win game menus
     gameOverMenu.style.visibility = "hidden";
@@ -242,193 +242,6 @@ function reset() {
 
     shuffle();
 
-    playMusic();
+    // playMusic();
   }, 100);
 }
-
-// setStartState();
-
-// function startGame() {
-//   shuffle();
-
-//   playMusic();
-
-//   cards.forEach((card) => card.addEventListener("click", flipCard));
-
-//   cards.forEach((card) => {
-//     if (card.classList.contains("flipped")) {
-//       card.classList.remove("flipped");
-//       card.src = "./images/back.jpg";
-//     }
-//   });
-
-//   let resetButton = document.querySelectorAll(".btn");
-//   resetButton.forEach((resetButton) => {
-//     resetButton.addEventListener("click", reset);
-//   });
-// }
-
-// function flipCard(event) {
-//   if (numFlipped < 2) {
-//     let card = event.target;
-//     if (card.dataset.matched === "true") {
-//       return;
-//     }
-//     if (firstChoice === null) {
-//       firstChoice = card;
-//       firstChoice.classList.add("flipped");
-//       sounds.flip.currentTime = 0;
-//       sounds.flip.play();
-//       firstChoice.src = `./images/${this.dataset.name}.jpg`;
-//       numFlipped++;
-//       // Remove click event listener from the firstChoice element.
-//       //   firstChoice.removeEventListener("click", flipCard);
-//       // Conditional below prevents the firstChoice card from being the secondChoice card as well
-//     } else if (secondChoice === null && card !== firstChoice) {
-//       secondChoice = card;
-//       secondChoice.classList.add("flipped");
-//       sounds.flip.currentTime = 0;
-//       sounds.flip.play();
-//       secondChoice.src = `./images/${this.dataset.name}.jpg`;
-//       numFlipped++;
-//       checkForMatch();
-//     }
-//   }
-// }
-
-// function checkForMatch() {
-//   if (firstChoice.dataset.name === secondChoice.dataset.name) {
-//     setTimeout(function () {
-//       firstChoice.dataset.matched = "true";
-//       secondChoice.dataset.matched = "true";
-//       matchedCounter();
-//       sounds.correct.currentTime = 0;
-//       sounds.correct.play();
-//       numFlipped = 0;
-//       firstChoice = null;
-//       secondChoice = null;
-//     }, 300);
-//   } else {
-//     setTimeout(function () {
-//       firstChoice.classList.remove("flipped");
-//       secondChoice.classList.remove("flipped");
-//       firstChoice.src = "./images/back.jpg";
-//       secondChoice.src = "./images/back.jpg";
-//       numFlipped = 0;
-//       firstChoice = null;
-//       secondChoice = null;
-//       livesCounter();
-//       sounds.incorrect.currentTime = 0;
-//       sounds.incorrect.play();
-//     }, 1000);
-//   }
-// }
-
-// function matchedCounter() {
-//   matched++;
-//   cardsMatched.textContent = matched;
-//   cardsMatched.style.color =
-//     matched === 8 ? "green" : matched >= 5 ? "yellow" : "";
-//   if (matched === 8 && matched == cards.length / 2) {
-//     cards.forEach((card) => card.removeEventListener("click", flipCard));
-//     setWinState();
-//     sounds.win.play();
-//   }
-// }
-
-// // Decrement function for lives remaining
-
-// function livesCounter() {
-//   lives--;
-//   livesLeft.textContent = lives;
-//   livesLeft.style.color = lives <= 0 ? "red" : lives <= 4 ? "orange" : "";
-//   if (lives <= 0) {
-//     cards.forEach((card) => card.removeEventListener("click", flipCard));
-//     setLoseState();
-//     sounds.lose.play();
-//   }
-// }
-
-// // Win and lose menu functions
-
-// // function setStartState() {
-// //   cards.forEach((card) => {
-// //     if (!card.classList.contains("flipped")) {
-// //       card.classList.add("flipped");
-// //       card.src = `./images/${card.dataset.name}.jpg`;
-// //       card.removeEventListener("click", flipCard);
-// //     }
-// //     startGameMenu.style.visibility = "visible";
-// //     startGameMenu.classList.add("show");
-// //   });
-// // }
-
-// function setWinState() {
-//   sounds.background.pause();
-//   setTimeout(function () {
-//     winGameMenu.style.visibility = "visible";
-//     winGameMenu.classList.add("show");
-//   }, 200);
-// }
-
-// function setLoseState() {
-//   cards.forEach((card) => {
-//     if (!card.classList.contains("flipped")) {
-//       card.classList.add("flipped");
-//       card.src = `./images/${card.dataset.name}.jpg`;
-//     }
-//   });
-//   sounds.background.pause();
-//   setTimeout(function () {
-//     gameOverMenu.style.visibility = "visible";
-//     gameOverMenu.classList.add("show");
-//   }, 200);
-// }
-
-// // Function for background music
-
-// function playMusic() {
-//   let backgroundMusic = sounds.background;
-//   backgroundMusic.play();
-// }
-
-// // Function for new game and reset
-
-// function reset() {
-//   setTimeout(function () {
-//     // flip all cards back over
-//     cards.forEach((card) => {
-//       card.classList.remove("flipped");
-//       card.dataset.matched = "false";
-//       card.src = "./images/back.jpg";
-//       card.addEventListener("click", flipCard);
-//     });
-
-//     // reset game variables
-//     firstChoice = null;
-//     secondChoice = null;
-//     numFlipped = 0;
-//     matched = 0;
-//     lives = 7;
-
-//     const menuVars = [
-//       { element: cardsMatched, value: matched, color: "white" },
-//       { element: livesLeft, value: lives, color: "white" },
-//     ];
-
-//     menuVars.forEach((varObj) => {
-//       varObj.element.style.color = varObj.color;
-//       varObj.element.textContent = varObj.value;
-//     });
-
-//     // hide game over and win game menus
-//     gameOverMenu.style.visibility = "hidden";
-//     winGameMenu.style.visibility = "hidden";
-
-//     // shuffle cards and play background music
-
-//     shuffle();
-
-//     // playMusic();
-//   }, 100);
-// }
